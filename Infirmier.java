c2241003d8e5ab38d89612922b858b4750e58a9d
Alannah2 <aepaulard@gmail.com>
2018-04-03 09:14:13 +0200

    dernier jour ppe

---
+++ Infirmier.java
@@ -0,0 +1,8 @@
+public class Infirmier extends Element
+{
+    INFIRMIER (#NUMERO, #CODE_SERVICE, ROTATION, SALAIRE)
+    numero decimal (4) primary key,
+    code_service char (3),
+    rotation enum ('JOUR', 'NUIT') not null default 'JOUR',
+    salaire decimal (6,2),
+}

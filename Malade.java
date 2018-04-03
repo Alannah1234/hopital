c10784003419ba810c192f2bc82dc29398088a66
Alannah2 <aepaulard@gmail.com>
2018-04-03 09:18:53 +0200

    Mme P

---
+++ Malade.java
@@ -0,0 +1,10 @@
+public class Malade extends Element
+{
+    MALADE (NUMERO, NOM, PRENOM, TEL, ADRESSE, MUTUELLE)
+    numero decimal (4) primary key,
+    nom varchar (12) not null,
+    prenom varchar (12) not null,
+    adresse varchar (40),
+    tel char (14),
+    mutuelle varchar (6),
+}
